6b8635580040e4a6fe47dbd3c1c2c333cb909a5c
MrYasin <mryasincengiz@gmail.com>
2019-05-18 01:30:48 +0300

    Added Menu Functions

--- Notepad.py
+++ Notepad.py
@@ -7,7 +7,8 @@
 
 import sys
 import os
-from PyQt5.QtWidgets import QWidget,QApplication,QTextEdit,QLabel,QPushButton,QVBoxLayout,QHBoxLayout,QFileDialog
+from PyQt5.QtWidgets import \
+    QWidget,QApplication,QTextEdit,QLabel,QPushButton,QVBoxLayout,QHBoxLayout,QFileDialog,QAction,qApp,QMainWindow
 
 class Notepad(QWidget):
 
@@ -42,7 +43,7 @@ class Notepad(QWidget):
         self.open.clicked.connect(self.open_file)
         self.save.clicked.connect(self.save_file)
 
-        self.show()
+
 
     def clear_text(self):
 
@@ -68,18 +69,56 @@ class Notepad(QWidget):
 
 
 
+class Menu(QMainWindow):
+
+    def __init__(self):
+
+        super().__init__()
+
+        self.window = Notepad()
+        self.setCentralWidget(self.window)
+
+        self.createMenu()
+
+    def createMenu(self):
+
+        menuBar = self.menuBar()
+
+        file = menuBar.addMenu("File")
+
+        openFile = QAction("Open File",self)
+        saveFile = QAction("Save File",self)
+        clearText = QAction("Clear Text",self)
+        exitFile = QAction("Exit", self)
+
+        openFile.setShortcut("Ctrl+O")
+        saveFile.setShortcut("Ctrl+S")
+        exitFile.setShortcut("Ctrl+Q")
+
+        file.addAction(openFile)
+        file.addAction(saveFile)
+        file.addAction(clearText)
+        file.addAction(exitFile)
 
 
+        file.triggered.connect(self.response)
+        self.setWindowTitle("Notepad(--)")
+        self.show()
 
+    def response(self, action):
 
+        if action.text() == "Open File":
+            self.window.open_file()
 
+        elif action.text() == "Save File":
+            self.window.save_file()
 
+        elif action.text() == "Clear Text":
+            self.window.clear_text()
 
-
-
-
+        elif action.text() == "Exit":
+            qApp.quit()
 
 app = QApplication(sys.argv)
-window = Notepad()
+menu = Menu()
 sys.exit(app.exec())
-
